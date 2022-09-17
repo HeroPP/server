1f4611a124e257c2f38b99976d8fdb671e9808d3
Jonas Curth <mail@jonascurth.de>
2022-09-17 20:31:21 +0200

    Nullable String

--- Hero.Server.Core/Models/Skill.cs
+++ Hero.Server.Core/Models/Skill.cs
@@ -3,7 +3,7 @@
     public class Skill
     {
         public Guid Id { get; set; }
-        public String AbilityName { get; set; }
+        public String? AbilityName { get; set; }
         public string IconUrl { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
