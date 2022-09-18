54d184112c2d8e7a054035e98650f150053900e3
Jonas Curth <mail@jonascurth.de>
2022-09-19 01:45:45 +0200

    fix undetected merge conflicts

--- Hero.Server/Controllers/NodeTreesController.cs
+++ Hero.Server/Controllers/NodeTreesController.cs
@@ -16,69 +16,70 @@ namespace Hero.Server.Controllers
     {
         private readonly INodeTreeRepository repository;
         private readonly IMapper mapper;
-        private readonly ILogger<NodeTreesController> logger;
 
         public NodeTreesController(INodeTreeRepository repository, IMapper mapper, ILogger<NodeTreesController> logger)
+            : base(logger)
         {
             this.repository = repository;
             this.mapper = mapper;
-            this.logger = logger;
         }
 
         [HttpGet("{id}")]
-        public Task<Response<NodeTreeResponse?>> GetNodeTreeByIdAsync(Guid id)
+        public Task<IActionResult> GetNodeTreeByIdAsync(Guid id)
         {
             return this.HandleExceptions(async () =>
             {
                 NodeTree? nodeTree = await this.repository.GetNodeTreeByIdAsync(id);
                 if (nodeTree != null)
                 {
-                    return this.mapper.Map<NodeTreeResponse>(nodeTree);
+                    return this.Ok(this.mapper.Map<NodeTreeResponse>(nodeTree));
                 }
 
-                return null;
+                return this.BadRequest();
             });
         }
 
         [HttpGet("{charId}")]
-        public Task<Response<List<NodeTreeOverviewResponse>>> GetNodeTreeOverviewsAsync(Guid charId)
+        public Task<IActionResult> GetNodeTreeOverviewsAsync(Guid charId)
         {
             return this.HandleExceptions(async () =>
             {
                 List<NodeTree> nodeTrees = (await this.repository.GetAllNodeTreesOfCharacterAsync(charId)).ToList();
 
-                return nodeTrees.Select(nodeTree => this.mapper.Map<NodeTreeOverviewResponse>(nodeTree)).ToList();
+                return this.Ok(nodeTrees.Select(nodeTree => this.mapper.Map<NodeTreeOverviewResponse>(nodeTree)).ToList());
             });
         }
 
         [HttpDelete("{id}")]
-        public Task<Response> DeleteNodeTreeAsync(Guid id)
+        public Task<IActionResult> DeleteNodeTreeAsync(Guid id)
         {
             return this.HandleExceptions(async () =>
             {
                 await this.repository.DeleteNodeTreeAsync(id);
+                return this.Ok();
             });
         }
 
         [HttpPut("{id}")]
-        public Task<Response> UpdateNodeTreeAsync(Guid id, [FromBody] CreateNodeTreeRequest request)
+        public Task<IActionResult> UpdateNodeTreeAsync(Guid id, [FromBody] CreateNodeTreeRequest request)
         {
             return this.HandleExceptions(async () =>
             {
                 NodeTree nodeTree = this.mapper.Map<NodeTree>(request);
                 await this.repository.UpdateNodeTreeAsync(id, nodeTree);
+                return this.Ok();
             });
         }
 
         [HttpPost]
-        public Task<Response<NodeTreeResponse>> CreateNodeTreeAsync([FromBody] CreateNodeTreeRequest request)
+        public Task<IActionResult> CreateNodeTreeAsync([FromBody] CreateNodeTreeRequest request)
         {
             return this.HandleExceptions(async () =>
             {
                 NodeTree nodeTree = this.mapper.Map<NodeTree>(request);
                 await this.repository.CreateNodeTreeAsync(nodeTree);
 
-                return this.mapper.Map<NodeTreeResponse>(nodeTree);
+                return this.Ok(this.mapper.Map<NodeTreeResponse>(nodeTree));
             });
         }
     }
