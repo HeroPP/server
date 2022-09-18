da66a90e19e99b4e5bdd613d45e62df79d270a08
JeremySoftware <113513756+JeremySoftware@users.noreply.github.com>
2022-09-18 14:06:17 +0200

    Fixed rights (#7)

--- Hero.Server/Controllers/AbilitiesController.cs
+++ Hero.Server/Controllers/AbilitiesController.cs
@@ -4,11 +4,12 @@ using Hero.Server.Core.Repositories;
 using Hero.Server.Identity;
 using Hero.Server.Messages.Requests;
 using Hero.Server.Messages.Responses;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hero.Server.Controllers
 {
-    [ApiController, Route("api/[controller]")]
+    [ApiController, Authorize(Roles = RoleNames.Administrator), Route("api/[controller]")]
     public class AbilitiesController : HeroControllerBase
     {
         private readonly IAbilityRepository repository;
