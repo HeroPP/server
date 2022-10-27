f314649838c6d95712c56e1b2335caf63eb50010
JeremySoftware <113513756+JeremySoftware@users.noreply.github.com>
2022-10-27 09:54:10 +0200

    Ability nullable (#14)
    * Ability nullable
    
    Set Ability to nullable field
    
    * Iconurl nullable
    
    For creating a iconless skill it is needed for the url to be nullable.
    
    * Abilities und Iconurl nullable

--- Hero.Server.Core/Models/Skill.cs
+++ Hero.Server.Core/Models/Skill.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public Guid GroupId { get; set; }
         public string? AbilityName { get; set; }
-        public string IconUrl { get; set; }
+        public string? IconUrl { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int HealthPointsBoost { get; set; }
