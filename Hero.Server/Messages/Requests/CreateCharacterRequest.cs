be0f0f68d7a9d46c3f41ca55ad7c8b8017dd9142
JeremySoftware <jeremy.koedding@gmail.com>
2022-11-14 14:41:27 +0100

    added response and requests

--- Hero.Server/Messages/Requests/CreateCharacterRequest.cs
+++ Hero.Server/Messages/Requests/CreateCharacterRequest.cs
@@ -8,6 +8,13 @@ namespace Hero.Server.Messages.Requests
         [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        public string? IconUrl { get; set; }
+        public int? Age { get; set; }
+        public string? Inventory { get; set; }
+        public string? Religion { get; set; }
+        public string? Relationship { get; set; }
+        public string? Notes { get; set; }
+        public string? Profession { get; set; }
         public Guid RaceId { get; set; }
     }
 }
--- Hero.Server/Messages/Responses/CharacterDetailResponse.cs
+++ Hero.Server/Messages/Responses/CharacterDetailResponse.cs
@@ -9,6 +9,13 @@ namespace Hero.Server.Messages.Responses
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+        public string? IconUrl { get; set; }
+        public int? Age { get; set; }
+        public string? Inventory { get; set; }
+        public string? Religion { get; set; }
+        public string? Relationship { get; set; }
+        public string? Notes { get; set; }
+        public string? Profession { get; set; }
         public RaceResponse Race { get; set; }
         public List<AttributeValueResponse> Attributes { get; set; }
         public List<SkilltreeResponse> SkilltreeResponses { get; set; }
