c27bdc07989a9d4b701f58dd27dd65af5cc9be61
JeremySoftware <113513756+JeremySoftware@users.noreply.github.com>
2022-11-14 15:26:23 +0100

    Added a few properties for character (#24)
    * Added a few properties for character, so that it has a little more "color".
    
    * added response and requests

--- Hero.Server.Core/Models/Character.cs
+++ Hero.Server.Core/Models/Character.cs
@@ -5,6 +5,13 @@
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public Guid GroupId { get; set; }
+        public string? IconUrl { get; set; }
+        public int? Age { get; set; }
+        public string? Inventory { get; set; }
+        public string? Religion { get; set; }
+        public string? Relationship { get; set; }
+        public string? Notes { get; set; }
+        public string? Profession { get; set; }
         public List<Skilltree> Skilltrees { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
@@ -16,6 +23,15 @@
             this.Name = character.Name;
             this.Description = character.Description;
             this.Skilltrees = character.Skilltrees;
+            this.Race = character.Race;
+            this.RaceId = character.RaceId;
+            this.Age = character.Age;
+            this.Inventory = character.Inventory;
+            this.Religion = character.Religion;
+            this.Relationship = character.Relationship;
+            this.Notes = character.Notes;
+            this.Profession = character.Profession;
+            this.IconUrl = character.IconUrl;
         }
     }
 }
